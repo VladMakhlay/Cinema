d837665e9e76fe023bc9cb0b98943cfb0d855d02
Vlad <vladm369@gmail.com>
2018-01-05 20:17:24 +0200

    trying importing react component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../../scss/style.scss';
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import printMe from './print';
-import './scss/index.scss';
+// import printMe from './print';
+// import  './scss/style.scss';
+//
+// function print() {
+//     let el = document.createElement('div');
+//     let btn = document.createElement('button');
+//
+//     el.classList.add('hello');
+//
+//     el.innerHTML = 'You have some results with webpack ';
+//     btn.innerText = 'Click me and check the console';
+//     btn.onclick = printMe;
+//
+//     el.appendChild(btn);
+//
+//     return el;
+// }
+// document.body.appendChild(print());
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function print() {
-    let el = document.createElement('div');
-    let btn = document.createElement('button');
+import Main from './components/Main/Main';
 
-    el.classList.add('hello');
 
-    el.innerHTML = 'You have some results with webpack ';
-    btn.innerText = 'Click me and check the console';
-    btn.onclick = printMe;
-
-    el.appendChild(btn);
-
-    return el;
-}
-
-document.body.appendChild(print());
+ReactDOM.render(
+    <Main/>,
+    document.getElementById('root')
+);
