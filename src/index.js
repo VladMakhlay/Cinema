394ec59807910b61e8e35c27e518eb29171b28df
Vlad <vladm369@gmail.com>
2018-01-13 19:28:18 +0200

    redux implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
 
 import './scss/style.scss';
 import Main from './components/Main/Main';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+export default store;
+
 const root = document.getElementById('root');
 ReactDOM.render(
-    <Main />,
+    <Provider store={store}>
+        <Main />
+    </Provider>,
     root,
 );
