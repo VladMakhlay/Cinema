52de1fdc36a18cbc4be60f9f365ccf5a624dc9ac
Vlad <vladm369@gmail.com>
2018-01-18 00:15:35 +0200

    filter for today's list of films

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,33 +2,23 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import loadMovies from '../../actions/moviesList';
-// import axios from 'axios';
 
 class MoviesList extends Component {
-    // constructor(props) {
-    //     super(props);
-    // this.state = {
-    //     movies: [],
-    // };
-    // this.loadMovies = this.loadMovies.bind(this);
-    // }
     componentDidMount() {
-        // this.loadMovies();
         this.props.loadMovies();
     }
 
-    // loadMovies() {
-    //     axios.get('http://demo4787444.mockable.io/posters')
-    //         .then((response) => {
-    //             this.setState({ movies: response.data });
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    // }
-
     render() {
-        const { movies } = this.props.moviesList;
+        let { movies } = this.props.moviesList;
+        const today = moment().format();
+        movies = movies.filter((movie) => {
+            let res;
+            if (moment(movie.start_date, 'DD-MM-YYYY').isSameOrBefore(today) &&
+                moment(movie.end_date, 'DD-MM-YYYY').isSameOrAfter(today)) {
+                res = movie;
+            }
+            return res;
+        });
         const poster = movies.map(movie => (
             <div className="b-movie" key={movie.id} >
                 <div className="b-movie__poster">
@@ -43,7 +33,7 @@ class MoviesList extends Component {
             <div className="b-moviesList">
                 <header className="b-moviesList__header">
                     <span className="b-moviesList__today">Today Shown</span>
-                    <span className="b-moviesList__date">{moment().format('D MMMM YYYY')}</span>
+                    <span className="b-moviesList__date">{moment().format('D MMMM Y')}</span>
                 </header>
                 <div className="b-moviesList__body">
                     {poster}
