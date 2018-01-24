14e57494b0c8cf4ac0210a7080c67d8e9d5cca06
Vlad <vladm369@gmail.com>
2018-01-24 17:15:50 +0200

    initial welcome page content'

--- src/scripts/components/Welcome/index.js
+++ src/scripts/components/Welcome/index.js
@@ -5,7 +5,22 @@ class Welcome extends Component {
         return (
             <section className="b-welcome">
                 <div className="b-greeting">
-                    <span>Welcome</span>
+                    <span><b className="gold">Cinema</b> salutes you</span>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <article className="b-about-us">
+                                <h2>You are welcome to:</h2>
+                                <ul>
+                                    <li className="b-about-us__li">Know what movies we are showing, and all the info about them</li>
+                                    <li className="b-about-us__li">Book a ticket(s) for today or for the next 7 days</li>
+                                    <li className="b-about-us__li">Select special seats for couples and small companies</li>
+                                    <li className="b-about-us__li">Know what pictures are coming soon</li>
+                                </ul>
+                            </article>
+                        </div>
+                    </div>
                 </div>
             </section>
         );
