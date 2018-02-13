dd4d3aebf13b73791ff24e27c7a06cb2e8993f40
Vlad <vladm369@gmail.com>
2018-02-13 17:01:00 +0200

    in hall(act) from http to https

--- src/scripts/actions/hall.js
+++ src/scripts/actions/hall.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { TAKEN_SEAT_FETCH, TAKEN_SEAT_FETCH_ERROR } from '../reducers/hall';
 
 const loadTakenSeats = () => (dispatch) => {
-    axios.get('http://demo4787444.mockable.io/seats')
+    axios.get('https://demo4787444.mockable.io/seats')
         .then((response) => {
             dispatch({ type: TAKEN_SEAT_FETCH, payload: response.data });
         })
